df6cf72f191107e91d49fcbaa9903941a7c7ccc2
chenwenyan <wychen2018@gmail.com>
2026-03-12 03:59:50 +0000

    Some ad-hoc changes needed in vLLM when using CacheBlend

--- vllm/v1/worker/gpu_worker.py
+++ vllm/v1/worker/gpu_worker.py
@@ -37,6 +37,9 @@ from vllm.v1.worker.worker_base import WorkerBase
 
 logger = init_logger(__name__)
 
+from lmcache.v1.compute.models.utils import VLLMModelTracker
+from lmcache.integration.vllm.utils import ENGINE_NAME
+
 if TYPE_CHECKING:
     from vllm.model_executor.model_loader.tensorizer import TensorizerConfig
     from vllm.v1.core.sched.output import SchedulerOutput
@@ -211,6 +214,8 @@ class Worker(WorkerBase):
         eep_scale_up = os.environ.get("VLLM_ELASTIC_EP_SCALE_UP_LAUNCH") == "1"
         with self._maybe_get_memory_pool_context(tag="weights"):
             self.model_runner.load_model(eep_scale_up=eep_scale_up)
+        VLLMModelTracker.register_model(ENGINE_NAME, self.model_runner.model)
+        ensure_kv_transfer_initialized(self.vllm_config)     
 
     def update_config(self, overrides: dict[str, Any]) -> None:
         self.model_runner.update_config(overrides)
@@ -707,4 +712,4 @@ def init_worker_distributed_environment(
         parallel_config.pipeline_parallel_size,
         parallel_config.decode_context_parallel_size)
 
-    ensure_kv_transfer_initialized(vllm_config)
+    # ensure_kv_transfer_initialized(vllm_config)
